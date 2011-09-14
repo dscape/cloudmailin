c5df8ce0e9e944991e5f2373bd4f6f0a0564187c
Nuno Job <nunojobpinto@gmail.com>
2011-09-14 22:57:23 +0100

    [server] removed debugging, fixed issue with \

--- server.js
+++ server.js
@@ -31,14 +31,13 @@ function email_route(request,response,next) {
           var encoded_field = fields[k]
             .replace(/"/g, '\\"')
             .replace(/'/g, '"')
-            .replace(/\n/g, "\ \n");
+            .replace(/\n/g, "\\ \n");
           return "-F '" + k + '=' + encoded_field  + "'";
       }).join(' ');
       subscribers.emit('email', parts);
       response.send('{ok: true}', 201); 
     }
     else {
-      _.keys(channels).forEach(function (k) { channels[k].emit('email', JSON.stringify(fields)) });
       response.send('{reason: "No subscribers", error: "no_subscribers"}', 200);
     }
   });
